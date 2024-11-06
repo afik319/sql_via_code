5f15d96631a6d73f54359d66f130c7eca5a6d2c6
Afik.r <Afik.r@credito.co.il>
2024-11-06 18:24:53 +0200

    errors details added

--- SQLViaCode.py
+++ SQLViaCode.py
@@ -12,12 +12,22 @@ def get_query_from_db(query):
     host = os.getenv("DB_HOST")
     dbname = os.getenv("DB_NAME")
     driver = os.getenv("DB_DRIVER")
+    try:
+        engine = create_engine(f"mssql+pyodbc://{user}:{password}@{host}/{dbname}?driver={driver}")
+        with engine.connect() as conn:
+            print("Connected to DB")
+    except Exception as e:
+        print("Error: Could not connect to the database. Please check if the username, password, host, or database name are correct.")
+        print("Error details:", e)
+        return None
 
-    engine = create_engine(f"mssql+pyodbc://{user}:{password}@{host}/{dbname}?driver={driver}")
-
-    print("Connected to DB")
-    with engine.connect() as conn:
-        output_query = conn.execute(text(query))
-        rows = output_query.fetchall()
-        columns_name = list(output_query.keys())
-    return pd.DataFrame(rows, columns=columns_name)
+    try:
+        with engine.connect() as conn:
+            output_query = conn.execute(text(query))
+            rows = output_query.fetchall()
+            columns_name = list(output_query.keys())
+        return pd.DataFrame(rows, columns=columns_name)
+    except Exception as e:
+        print("Error executing query")
+        print("Error details:", e)
+        return None
